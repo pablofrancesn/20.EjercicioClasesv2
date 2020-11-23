1d43a1e8896aedc951c9fe0ec956a559e03292d6
Pablo Francés Navarro <pablofrances.n@gmail.com>
2020-11-23 09:58:50 +0100

    Inicio

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sn = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+		CazurroLeones[] cazurros = new CazurroLeones[500];
+		VecinaRubia[] vecinas = new VecinaRubia[500];
+		for (int i = 0; i < args.length; i++) {
+			System.out.print("Introduce nombre: ");
+			String name = sc.nextLine();
+		}
+	}
+
+}
